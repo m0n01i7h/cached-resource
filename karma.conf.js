736fe6e3aa05839dd06dabb505bf86af77bcd561
Viktor Zhakhalov <zhakhalov@gmail.com>
2017-04-12 23:24:35 +0300

    Cover utils with tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration
-// Generated on Mon Apr 10 2017 23:17:30 GMT+0300 (Финляндия (лето))
-
 var webpackConfig = require('./webpack.config.js');
 
 module.exports = function (config) {
@@ -16,26 +13,9 @@ module.exports = function (config) {
     preprocessors: {
       '**/*.ts': ['karma-typescript']
     },
-    // mime: {
-    //   'text/x-script': ['ts', 'tsx']
-    // },
     reporters: ['progress', 'karma-typescript'],
     port: 9876,
     colors: true,
-    webpack: webpackConfig,
-    // webpack: {
-    //   devtool: 'inline-source-map',
-    //   // debug: true,
-    //   resolve: webpackConfig.resolve,
-    //   module: webpackConfig.module,
-    // },
-    webpackMiddleware: {
-      // quiet: true,
-      stats: {
-        colors: true,
-        errorDetails: true,
-      }
-    },
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,17 +1,99 @@
 import { expect } from 'chai';
-import { randomString } from '../lib/utils';
+import {
+  randomString,
+  getInstanceKey,
+  getActionKey,
+  getArrayKey,
+  getUrl,
+  getRandomParams,
+  defer
+} from '../lib/utils';
 
-describe('#randomString()', () => {
-  it('should generate given length', () => {
-    const given = 15;
-    expect(randomString(given).length).to.be.equal(given);
+describe('Utils', () => {
+  describe('#randomString()', () => {
+    it('should generate given length', () => {
+      const given = 15;
+      expect(randomString(given).length).to.be.equal(given);
+    });
+
+    it('should generate default length', () => {
+      expect(randomString().length).to.be.equal(12);
+    });
+
+    it('should contain alphanumeric', () => {
+      expect(/^[a-zA-Z0-9]*$/.test(randomString())).to.be.true;
+    });
   });
 
-  it('should generate default length', () => {
-    expect(randomString().length).to.be.equal(12);
+  describe('#getActionKey()', () => {
+    it('should generate proper key', () => {
+      expect(getActionKey({ a: 5, b: 'hello' })).to.be.equal('action?a=5&b=hello');
+    });
   });
 
-  it('should contain alphanumeric', () => {
-    expect(/^[a-zA-Z0-9]*$/.test(randomString())).to.be.true;
+  describe('#getArrayKey()', () => {
+    it('should generate proper key', () => {
+      expect(getArrayKey({ a: 5, b: 'hello' })).to.be.equal('array?a=5&b=hello');
+    });
+  });
+
+  describe('#getInstanceKey()', () => {
+    it('should provide empty instance key', () => {
+      const given = { testParam: 'testValue' };
+
+      expect(getInstanceKey({ url: '' }, given)).to.be.equal('instance?');
+    });
+
+    it('should provide instance key by given params', () => {
+      const params = { testParam1: '@testParam', testParam2: 'test' };
+      const given = { testParam: 'testValue' };
+
+      expect(getInstanceKey({ url: '', params }, given)).to.be.equal('instance?testParam1=testValue&testParam2=test');
+    });
+  });
+
+  describe('#getUrl()', () => {
+    it('should provide url without params', () => {
+      expect(getUrl('/test-url', {})).to.be.equal('/test-url');
+    });
+
+    it('should provide url with params', () => {
+      expect(getUrl('/test-url/:testParam', { testParam: 'testValue' })).to.be.equal('/test-url/testValue');
+    });
+
+    it('should provide url with query', () => {
+      expect(getUrl('/test-url', { testParam: 'testValue' })).to.be.equal('/test-url?testParam=testValue');
+    });
+  });
+
+  describe('#getRandomParams()', () => {
+    it('should provide plain params', () => {
+      const given = { testParam1: 10, testParam2: 'test' };
+      const expected = { testParam1: 10, testParam2: 'test' };
+
+      expect(getRandomParams(given)).to.be.deep.equal(expected);
+    });
+
+    it('should provide params from source', () => {
+      const params = { testParam1: '@testParam', testParam2: 'test' };
+      const given = { testParam: 'testValue' };
+      const expected = { testParam1: 'testValue', testParam2: 'test' };
+
+      expect(getRandomParams(params, given)).to.be.deep.equal(expected);
+    });
+  });
+
+  describe('#defer()', () => {
+    it('should resolve', (done) => {
+      const def = defer();
+      def.promise.then(done);
+      def.resolve();
+    });
+
+    it('should reject', (done) => {
+      const def = defer();
+      def.promise.catch(done);
+      def.reject();
+    });
   });
 });
