4f56d9a8435f636986bbce3300b9a99b64171c58
Viktor Zhakhalov <zhakhalov@gmail.com>
2017-04-12 10:34:51 +0300

    Move to PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     },
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     singleRun: false,
     concurrency: Infinity,
   })
